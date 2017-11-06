0d0e5c2b28a073b1bdbd7b1bcb60fb85db4101f4
samizdam <samizdam@inbox.ru>
2017-11-06 17:35:00 +0300

    Prepare build prod docker image. Source env in build prod script.

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,7 @@
+# Base image: should be extended to dev and prod versions
+
 FROM php:7.1.10-cli-jessie
 
 WORKDIR /srv/rest-auth
+
+CMD ["php bin/server.php"]
--- tools/build-prod.sh
+++ tools/build-prod.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
+source .env
+
 FROM_IMAGE_NAME=${DOCKER_IMAGE}-base
+
 docker build \
     --build-arg ${FROM_IMAGE_NAME} \
     -f prod.Dockerfile \
