39a09b796cbd0615e512e16213b8ec09f906e0db
samizdam <samizdam@inbox.ru>
2017-11-06 17:54:46 +0300

    Prepare first release.

--- tools/build-prod.sh
+++ tools/build-prod.sh
@@ -5,6 +5,6 @@ source .env
 FROM_IMAGE_NAME=${DOCKER_IMAGE}-base
 
 docker build \
-    --build-arg ${FROM_IMAGE_NAME} \
+    --build-arg FROM_IMAGE_NAME=${FROM_IMAGE_NAME} \
     -f prod.Dockerfile \
     -t ${DOCKER_IMAGE} .
