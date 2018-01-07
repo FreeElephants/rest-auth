d0e4814d6090a089903a0d54c75704f7e1d9f806
samizdam <samizdam@inbox.ru>
2018-01-07 21:38:06 +0300

    v0.1.0

--- config/const.php
+++ config/const.php
@@ -8,7 +8,7 @@
 define('REST_AUTH_DB_CONNECTION_URL', getenv('DB_CONNECTION_URL') ?: 'sqlite:////srv/rest-auth/db.sqlite');
 define('REST_AUTH_DEV_MODE', explode(',', getenv('REST_AUTH_DEV_MODE') ?: true));
 define('REST_AUTH_HTTP_HOST', getenv('REST_AUTH_HTTP_HOST') ?: '127.0.0.1');
-define('REST_AUTH_PORT', getenv('REST_AUTH_PORT') ?: 8080);
+define('REST_AUTH_PORT', getenv('REST_AUTH_PORT') ?: 8080); // TODO make static - can be mapped with docker
 define('REST_AUTH_ADDRESS', getenv('REST_AUTH_ADDRESS') ?: '0.0.0.0');
 define('REST_AUTH_ORIGIN', explode(',', getenv('REST_AUTH_ORIGIN') ?: '*'));
 define('REST_JWT_KEY', explode(',', getenv('REST_JWT_KEY') ?: 'secret'));
--- tools/build-base.sh
+++ tools/build-base.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
 source .env
-
+if test; then
+    echo 1
+   else
+    ecpo 2
+fi
 docker build -t ${DOCKER_IMAGE}-base .
